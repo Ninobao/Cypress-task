88decf15b3ba5bada26eb7ec1ec9e69fa349ae3f
Eric RuizFlores Lopez <eric.rfl7@gmail.com>
2025-11-27 18:32:42 -0600

    Moved the custom commands to commands.js. Changed the name of custom command searchInSearchBar for searchInSearchbar

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -2,14 +2,10 @@ describe("GitHub web page basic test cases", () => {
   it("Should send the correct credentials when the user logins", () => {
     cy.visit("/login");
 
-    Cypress.Commands.add("enterCredentialsLogin", (email, password) => {
-      cy.get("input#login_field").type(email);
-      cy.get("input#password").type(password);
-    });
-
     cy.intercept("POST", "https://github.com/session").as("loginRequest");
 
     cy.enterCredentialsLogin("fakeuser@example.com", "123_password");
+
     cy.get('[data-signin-label="Sign in"]').click();
 
     cy.wait("@loginRequest").then((interception) => {
@@ -25,13 +21,7 @@ describe("GitHub web page basic test cases", () => {
 
     cy.intercept("GET", "/search*").as("searchInput");
 
-    Cypress.Commands.add("searchInSearchBar", (input) => {
-      cy.get('button[aria-label="Toggle navigation"] span.Button-content').click();
-      cy.get(".search-input-container").click();
-      cy.get('[data-target="query-builder.input"]').type(`${input}{enter}`);
-    });
-
-    cy.searchInSearchBar("gitignore");
+    cy.searchInSearchbar("gitignore");
 
     cy.wait("@searchInput").then((intercept) => {
       expect(intercept.request.url).to.include("q=gitignore");
@@ -42,11 +32,6 @@ describe("GitHub web page basic test cases", () => {
     cy.visit("/");
 
     cy.intercept("GET", "https://github.com/pricing").as("navigateToPricing");
-
-    Cypress.Commands.add("clickOnMenuElement", (input) => {
-      cy.get('button[aria-label="Toggle navigation"] span.Button-content').click();
-      cy.contains(input).click();
-    });
 
     cy.clickOnMenuElement("Pricing");
 
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,25 +1,15 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+Cypress.Commands.add("enterCredentialsLogin", (email, password) => {
+  cy.get("input#login_field").type(email);
+  cy.get("input#password").type(password);
+});
+
+Cypress.Commands.add("searchInSearchbar", (input) => {
+  cy.get('button[aria-label="Toggle navigation"] span.Button-content').click();
+  cy.get(".search-input-container").click();
+  cy.get('[data-target="query-builder.input"]').type(`${input}{enter}`);
+});
+
+Cypress.Commands.add("clickOnMenuElement", (input) => {
+  cy.get('button[aria-label="Toggle navigation"] span.Button-content').click();
+  cy.contains(input).click();
+});

5c4a4ce8462b7010ae99fa7a0d711634d0647515
Eric RuizFlores Lopez <eric.rfl7@gmail.com>
2025-12-03 14:29:35 -0600

    Improved assertions in test cases

--- cypress/e2e/github.cy.js
+++ cypress/e2e/github.cy.js
@@ -35,7 +35,10 @@ describe("GitHub web page basic test cases", () => {
     cy.searchInSearchbar("gitignore");
 
     cy.wait("@searchInput").then((intercept) => {
-      expect(intercept.request.url).to.include("q=gitignore");
+      const url = new URL(intercept.request.url);
+
+      expect(url.pathname).to.equal("/search");
+      expect(url.searchParams.get("q")).to.equal("gitignore");
     });
   });
 
@@ -47,7 +50,10 @@ describe("GitHub web page basic test cases", () => {
     cy.clickOnMenuElement("Pricing");
 
     cy.wait("@navigateToPricing").then((intercept) => {
-      expect(intercept.request.url).to.equal("https://github.com/pricing");
+      const url = new URL(intercept.request.url);
+
+      expect(intercept.request.method).to.equal("GET");
+      expect(url.pathname).to.equal("/pricing");
     });
   });
 
@@ -60,11 +66,12 @@ describe("GitHub web page basic test cases", () => {
 
     cy.wait("@getBranches").then((intercept) => {
       expect(intercept.response.body).to.have.property("branches");
+      expect(intercept.response.body.branches).to.be.a("number");
     });
   });
 
   it("The repository page should contain a README file", () => {
     cy.visit("/github/gitignore");
-    cy.get(repositoryPage.readme).should("exist");
+    cy.get(repositoryPage.readme).should("exist").and("be.visible");
   });
 });
